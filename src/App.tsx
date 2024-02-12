41aecbc76aa12d6147bd507a20067f7993c325e4
joannacharles <122937742+joannacharles@users.noreply.github.com>
2024-02-12 10:51:47 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,103 @@
+import { Fragment, useCallback, useEffect, useMemo, useState } from "react"
+import { InputSelect } from "./components/InputSelect"
+import { Instructions } from "./components/Instructions"
+import { Transactions } from "./components/Transactions"
+import { useEmployees } from "./hooks/useEmployees"
+import { usePaginatedTransactions } from "./hooks/usePaginatedTransactions"
+import { useTransactionsByEmployee } from "./hooks/useTransactionsByEmployee"
+import { EMPTY_EMPLOYEE } from "./utils/constants"
+import { Employee } from "./utils/types"
+
+export function App() {
+  const { data: employees, ...employeeUtils } = useEmployees()
+  const { data: paginatedTransactions, ...paginatedTransactionsUtils } = usePaginatedTransactions()
+  const { data: transactionsByEmployee, ...transactionsByEmployeeUtils } = useTransactionsByEmployee()
+  const [isLoading, setIsLoading] = useState(false)
+  const [viewMoreEnable,setViewMoreEnable]=useState(true)
+  console.log("JOanna", paginatedTransactions )
+  
+  const transactions = useMemo(
+    () => paginatedTransactions?.data ?? transactionsByEmployee ?? null,
+    [paginatedTransactions, transactionsByEmployee]
+  )
+
+  const loadAllTransactions = useCallback(async () => {
+    setIsLoading(true)
+    transactionsByEmployeeUtils.invalidateData()
+    setIsLoading(false)
+    
+    
+    await employeeUtils.fetchAll()
+    await paginatedTransactionsUtils.fetchAll()
+    if(paginatedTransactions?.nextPage===null){
+      setViewMoreEnable(false)
+    }
+    else{
+      setViewMoreEnable(true)
+    }
+  }, [employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils])
+
+  const loadTransactionsByEmployee = useCallback(
+    async (employeeId: string) => {
+      setViewMoreEnable(false)
+      paginatedTransactionsUtils.invalidateData()
+      await transactionsByEmployeeUtils.fetchById(employeeId)
+    },
+    [paginatedTransactionsUtils, transactionsByEmployeeUtils]
+  )
+
+  useEffect(() => {
+    if (employees === null && !employeeUtils.loading) {
+      loadAllTransactions()
+    }
+  }, [employeeUtils.loading, employees, loadAllTransactions])
+
+  return (
+    <Fragment>
+      <main className="MainContainer">
+        <Instructions />
+
+        <hr className="KaizntreeBreak--l" />
+
+        <InputSelect<Employee>
+          isLoading={isLoading}
+          defaultValue={EMPTY_EMPLOYEE}
+          items={employees === null ? [] : [EMPTY_EMPLOYEE, ...employees]}
+          label="Filter by employee"
+          loadingLabel="Loading employees"
+          parseItem={(item) => ({
+            value: item.id,
+            label: `${item.firstName} ${item.lastName}`,
+          })}
+          onChange={async (newValue) => {
+            console.log("NEW VAL NULL JOANNA",newValue)
+
+            if (newValue === null) {
+              return
+            }
+
+            await loadTransactionsByEmployee(newValue.id)
+          }}
+        />
+
+        <div className="KaizntreeBreak--l" />
+
+        <div className="KaizntreeGrid">
+          <Transactions transactions={transactions} />
+
+          {transactions !== null && (
+            <button
+              className="KaizntreeButton"
+              disabled={paginatedTransactionsUtils.loading || !viewMoreEnable || paginatedTransactions?.nextPage===null} 
+              onClick={async () => {
+                await loadAllTransactions()
+              }}
+            >
+              View More
+            </button>
+          )}
+        </div>
+      </main>
+    </Fragment>
+  )
+}
---
+++ src/utils/requests.ts
@@ -0,0 +1,71 @@
+import {
+  PaginatedRequestParams,
+  PaginatedResponse,
+  RequestByEmployeeParams,
+  SetTransactionApprovalParams,
+  Transaction,
+  Employee,
+} from "./types"
+import mockData from "../mock-data.json"
+import { start } from "repl";
+import { Transactions } from "src/components/Transactions";
+import {App} from "src/App"
+
+const TRANSACTIONS_PER_PAGE = 5
+
+const data: { employees: Employee[]; transactions: Transaction[] } = {
+  employees: mockData.employees,
+  transactions: mockData.transactions,
+}
+
+export const getEmployees = (): Employee[] => data.employees
+
+export const getTransactionsPaginated = ({
+  page,
+}: PaginatedRequestParams): PaginatedResponse<Transaction[]> => {
+  if (page === null) {
+    return {
+      nextPage:null,
+      data: [...data.transactions],
+    } //jo
+    throw new Error("Page cannot be null")
+  }
+  
+  console.log(page)
+  const start = 0//page * TRANSACTIONS_PER_PAGE
+  const end = start + ((page+1)*TRANSACTIONS_PER_PAGE)
+
+  if (start > data.transactions.length) {
+    throw new Error(`Invalid page ${page}`)
+  }
+
+  const nextPage = end < data.transactions.length ? page + 1 : null
+
+  return {
+    nextPage,
+    data: data.transactions.slice(start, end),
+  }
+}
+
+export const getTransactionsByEmployee = ({ employeeId }: RequestByEmployeeParams) => {
+  
+  if (!employeeId) {
+   console.log(data.transactions)
+   //throw new Error("Employee id cannot be empty")
+   return [...data.transactions]
+  }
+
+  return data.transactions.filter((transaction) => transaction.employee.id === employeeId)
+}
+
+export const setTransactionApproval = ({ transactionId, value }: SetTransactionApprovalParams): void => {
+  const transaction = data.transactions.find(
+    (currentTransaction) => currentTransaction.id === transactionId
+  )
+
+  if (!transaction) {
+    throw new Error("Invalid transaction to approve")
+  }
+
+  transaction.approved = value
+}
